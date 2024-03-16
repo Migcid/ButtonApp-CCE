f7c802ad324d1b98f5f19a7fe086da0551c25a81
Miguel Cid Y <miguelcidyevenes@gmail.com>
2024-03-16 00:59:43 -0300

    agregando imagen a splash screen, dando color a background

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#D8E30E',
+    backgroundColor: '#84b6f4',
     alignItems: 'center',
     justifyContent: 'center',
   },
